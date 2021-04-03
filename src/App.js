4640a3c4291e763b3da5202397ce7663bd18d323
chahyadin <chahyadin@gmail.com>
2021-04-03 15:06:00 +0700

    Migrasi Komponen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
+import Browse from './components/Browse.js';
+import Arrived from './components/Arrived.js';
+import Clients from './components/Clients.js';
+import AsideMenu from './components/AsideMenu.js';
+import Footer from './components/Footer.js';
+
 function App() {
   return (
-    <div className="max-w-md mx=auto flex p-6 bg-gray-100 mt-10 rounded-lg">
-      <div calssName="ml-6 pt-1">
-        <hi calssName="text-2xl text-blue-700 leading-tight">
-          Tailwind and  Create React App
-        </hi>
-      <p ClassName="text-base text-gray-700 leading-normal">
-        Building apps together
-      </p>
-      </div>
-     </div>
+    <>
+     <Header />
+     <Hero />
+     <Browse />
+     <Arrived />
+     <Clients />
+     <AsideMenu />
+     <Footer />
+    </>
   );
 }
 
